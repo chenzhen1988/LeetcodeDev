5fad3a47debdacc0600dc1a7c0fb0e54934f73d6
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-01-26 14:27:42 +0800

    Create fmin.h

---
+++ src/libs/common/fmin.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description:    fmin
+ */
+
+#ifndef _FMIN_H_
+#define _FMIN_H_
+
+#define fmin(x, y) ((x) < (y) ? (x) : (y))
+
+#endif
