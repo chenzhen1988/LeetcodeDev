371b690fbde7bbddc1b899ef70f707a3b6d7e7e6
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-01-26 10:24:18 +0800

    Create fmax.h

---
+++ src/libs/common/fmax.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description:    fmax
+ */
+
+#ifndef _FMAX_H_
+#define _FMAX_H_
+
+#define fmax(x, y) ((x) > (y) ? (x) : (y))
+
+#endif
