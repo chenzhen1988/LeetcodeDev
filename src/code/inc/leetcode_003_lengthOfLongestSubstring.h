015659ed1c0193c62afc20d3d1c38660dd5749e6
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-02-10 14:20:33 +0800

    Create leetcode_003_lengthOfLongestSubstring.h

---
+++ src/code/inc/leetcode_003_lengthOfLongestSubstring.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description:    leetcode_003_lengthOfLongestSubstring
+ */
+#ifndef _LEETCODE_003_LENGTHOFLONGESTSUBSTRING_H_
+#define _LEETCODE_003_LENGTHOFLONGESTSUBSTRING_H_
+
+#include "stdbool.h"
+
+int lengthOfLongestSubstring(char * s);
+
+#endif
