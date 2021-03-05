384260ab6e9671b7ece5991b16e047fa3b56235a
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-03-05 15:33:16 +0800

    Create leetcode_1576_modify_string.h

---
+++ src/code/inc/leetcode_1576_modify_string.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description:    leetcode_1576_modify_string
+ */
+#ifndef _LEETCODE_1576_MODIFY_STRING_H_
+#define _LEETCODE_1576_MODIFY_STRING_H_
+
+#include "stdbool.h"
+
+char * modifyString(char * s);
+
+#endif
