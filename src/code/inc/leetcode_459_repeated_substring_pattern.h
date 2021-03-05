ef3f1e5364ba5290472975cea4c4eecc4b9cb8c8
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-03-05 16:31:17 +0800

    Create leetcode_459_repeated_substring_pattern.h

---
+++ src/code/inc/leetcode_459_repeated_substring_pattern.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description:    leetcode_459_repeated_substring_pattern
+ */
+#ifndef _LEETCODE_459_REPEATED_SUBSTRING_PATTERN_H_
+#define _LEETCODE_459_REPEATED_SUBSTRING_PATTERN_H_
+
+#include "stdbool.h"
+
+bool repeatedSubstringPattern(char * s);
+
+#endif
