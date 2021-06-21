c08225d482d821d9cdd8f37da9646ea934c63c5d
chenzhen1988 <77054125+chenzhen1988@users.noreply.github.com>
2021-06-21 14:49:15 +0800

    Update InitialCoverage.sh

--- InitialCoverage.sh
+++ InitialCoverage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "start initialize lcov coverage"
+rm -rf Coverage
+mkdir Coverage
 lcov -d build_scons -z
 lcov -d build_scons -b . --no-external --initial -c -o ./Coverage/CMakeGcovSupportInitialCoverage.info
 
